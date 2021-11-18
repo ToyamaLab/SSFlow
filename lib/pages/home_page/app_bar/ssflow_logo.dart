87342a0e349f6333c9c6825dc941be505bd14b98
Kuri-Tai <kuri@db.ics.keio.ac.jp>
2021-11-19 03:23:28 +0900

    issue-20 refactor: remove comments in pubspec.yaml

---
+++ lib/pages/home_page/app_bar/ssflow_logo.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:ssflow/providers/_providers.dart';
+import 'package:ssflow/utils/constants/_constants.dart';
+
+class SSFlowLogo extends ConsumerWidget {
+  SSFlowLogo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Container(
+      height: ref.watch(windowSize).height / 10,
+      decoration: BoxDecoration(
+        border: Border(
+          bottom: BorderSide(
+            color: SSColor.grey,
+            width: 1.0,
+          ),
+        ),
+        color: SSColor.black,
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SizedBox(width: ref.watch(windowSize).width * 0.01),
+              RichText(
+                text: TextSpan(
+                  text: 'S',
+                  style: TextStyle(
+                    color: SSColor.primary,
+                    fontSize: 32,
+                  ),
+                  children: <TextSpan>[
+                    TextSpan(
+                      text: 'SFlow',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 32,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/providers/size.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final windowSize = StateNotifierProvider<_SizeController, Size>(
+  (ref) => _SizeController(),
+);
+final leftSideAreaSize = StateNotifierProvider<_SizeController, Size>(
+  (ref) => _SizeController(),
+);
+final canvasAreaSize = StateNotifierProvider<_SizeController, Size>(
+  (ref) => _SizeController(),
+);
+final attributeAreaSize = StateNotifierProvider<_SizeController, Size>(
+  (ref) => _SizeController(),
+);
+
+class _SizeController extends StateNotifier<Size> {
+  _SizeController() : super(Size.zero);
+
+  Future<void> init(BuildContext context, {
+    Size? size,
+  }) async {
+    if (size != null) {
+      state = size;
+      return;
+    }
+
+    state = MediaQuery.of(context).size;
+  }
+}
